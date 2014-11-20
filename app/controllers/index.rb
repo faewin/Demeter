38957c21e6623afbd107e272549ce059e784b5bc
Eddie  & Sam David <pair+feek1g.sfd1987@devbootcamp.com>
2014-11-20 12:11:19 -0800

    login and signup added, user and session routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,6 +7,16 @@ get '/sessions/new' do
   erb :sign_in
 end
 
+post '/sessions' do
+  @valid_user = User.find_by(email: params[:email])
+  if @valid_user != nil && @valid_user.password == params[:password]
+    session[:user_id] = @valid_user.id
+    redirect '/twits' #redirect to twit feed
+  else
+    redirect 'users/new'
+  end
+end
+
 get '/users/new' do
   erb :sign_up_page
 end
@@ -18,4 +28,12 @@ post '/users' do
     else
       redirect '/user/new'
     end
+end
+
+# ********************************
+
+get '/twits' do
+
+  redirect 'users/new' unless session[:user_id]
+  erb :twits_feed
 end
