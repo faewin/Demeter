060c84d7dead8d187629a23056d9c22f3b02e896
Ryan Stong <rvanstong@gmail.com>
2014-11-20 11:57:56 -0800

    Updated models and migrations with validations. Seeded database. Dummy users' password is 'password'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   include BCrypt
 
+  has_many :twits
+  validates :name, :email, :password_hash, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/
+
   def password
     @password ||= Password.new(password_hash)
   end
