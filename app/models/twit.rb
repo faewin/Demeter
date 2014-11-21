c47538cd72bd365c069150311198620d41ffa603
Eddie  & Sam David <pair+feek1g.sfd1987@devbootcamp.com>
2014-11-21 15:06:56 -0800

    added validations for following

--- app/models/twit.rb
+++ app/models/twit.rb
@@ -1,5 +1,7 @@
 class Twit < ActiveRecord::Base
   # Remember to create a migration!
   validates_length_of :message, maximum: 140
+  validates_length_of :message, minimum: 1
+
   validates :user_id, presence: true
 end
