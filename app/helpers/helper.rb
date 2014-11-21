c61971ee63a2f714672061dee188e628cafbe517
Eddie  & Sam David <pair+feek1g.sfd1987@devbootcamp.com>
2014-11-21 15:23:25 -0800

    added api to email you, after you sign up

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -6,4 +6,21 @@ helpers do
     # image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
+  def pony(user_email)
+    Pony.mail({
+  :to => user_email,
+  :subject => "Welcome!",
+  :body => "Welcome to Twittastrophe!!!",
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'twittertest109@gmail.com',
+    :password             => 'ilovecakes123',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+})
+  end
 end
